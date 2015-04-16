8a4491ce6c85664f3649a970308b266eba33f753
Alex Ellis <AlexEllis2@gmail.com>
2015-04-16 08:40:30 +0100

    date correct

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('energy.db');
 
 var get_data = function(write_back) {
-	var query="select sum(kw_h) as total, strftime('%H', energy_time) as hour from energy where energy_date between date('2015-04-15') and date('now') group by strftime('%H', energy_time);";
+	var query="select sum(kw_h) as total, strftime('%H', energy_time) as hour from energy where energy_date between date('now','start of day') and date('now') group by strftime('%H', energy_time);";
 	db.serialize(function() {
 		db.all(query, function(err,rows) {
 			write_back(rows);
@@ -18,18 +18,18 @@ exports.overview = function(req,res) {
 		results.forEach(function(r){
 			total += Number(r.total);
 		});
-		var highest=undefined;
+		var highest = undefined;
 		results.forEach(function(r){
 			if(!highest) {
-				highest=r;
-			}else {
-				if(r.total>highest.total) {
-					highest=r;
+				highest = r;
+			} else {
+				if(r.total > highest.total) {
+					highest = r;
 				}
 			}
 		});
 
-		var comparator= function(a, b) {
+		var comparator = function(a, b) {
 			if(a.hour > b.hour){
 				return 1;
 			}
