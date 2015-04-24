1d1bfbd03952d8a6034825a3ddbfadb0926c3291
Alex Ellis <AlexEllis2@gmail.com>
2015-04-24 11:34:16 +0100

    Stats page

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var path = require('path');
 
 var app = express();
 
-
 app.set('view engine','vash');
 app.disable('etag');
 
@@ -15,13 +14,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.set('port', process.env.PORT || 8000);
 
-app.get('/', routes.overview);
-app.get('/history/:days', routes.overview);
-app.get('/graph/:days', routes.graph);
+app.get('/stats/', routes.stats);
+app.get('/history/:days/', routes.overview);
+app.get('/graph/:days/', routes.graph);
 app.use('/bower_components', express.static(path.join(__dirname,'/bower_components')));
 app.use('/style', express.static(path.join(__dirname,'/style')));
 app.use('/script', express.static(path.join(__dirname,'/script')));
 
+app.get('/', routes.overview);
+
 app.listen(app.get('port'), function() {
 	console.log("Listening on port " +app.get('port')) ;
 });
+
--- routes/api.js
+++ routes/api.js
@@ -1,59 +1,55 @@
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('energy.db');
+var repository=require('./repository');
 
-var get_data = function(offset, write_back) {
-	var modifier='';
-	if(offset>0) {
-		modifier = ',\'-'+offset+' days\'';
+var comparator = function(a, b) {
+	if(a.hour > b.hour){
+		return 1;
 	}
-	var query="select sum(kw_h) as total, strftime('%H', energy_time) as hour from energy "+
-	"where energy_date = date('now'"+modifier+") "+
-	"group by strftime('%H', energy_time);";
-	// console.log(query);
-	db.serialize(function() {
-		db.all(query, function(err,rows) {
-			if(err){
-				console.log(err);
-			}
-			// console.log(rows);
-			write_back(offset, rows);
-		});
-	});
+	if(a.hour < b.hour){
+		return -1;
+	}
+	return 0;
 };
 
-	var describe_offset = function(offset) {
-		if(offset==0){
-			return 'today';
-		}
-		else if(offset==1) {
-			return "yesterday";
-		}
-		return offset + " days ago";
-	};
-
-	var comparator = function(a, b) {
-		if(a.hour > b.hour){
-			return 1;
-		}
-		if(a.hour < b.hour){
-			return -1;
-		}
-		return 0;
-	};
+var describe_offset = function(offset) {
+	if(offset==0){
+		return 'today';
+	}
+	else if(offset==1) {
+		return "yesterday";
+	}
+	return offset + " days ago";
+};
 
 var get_highest_value =function(results){
-		var highest = undefined;
-		results.forEach(function(r){
-			if(!highest) {
+	var highest = undefined;
+	results.forEach(function(r){
+		if(!highest) {
+			highest = r;
+		} else {
+			if(r.total > highest.total) {
 				highest = r;
-			} else {
-				if(r.total > highest.total) {
-					highest = r;
-				}
 			}
-		});
-		return highest;
+		}
+	});
+	return highest;
 };
+
+
+exports.stats = function(req,res) {
+	var write_back=function (totals) {
+		res.render('stats',  
+			{
+				title: "Energy usage",
+				totalEnergy : totals.totalEnergy,
+				totalEnergyWeek: totals.totalEnergyWeek,
+				totalEnergyHour: totals.totalEnergyHour
+			}
+		);
+	};
+	repository.get_stats(write_back);
+};
+
+
 exports.overview = function(req,res) {
 	var write_back = function(dayOffset, results){ 
 		var total=0;
@@ -74,7 +70,7 @@ exports.overview = function(req,res) {
 		if(req.params&&req.params.days) {
 			dayOffset=req.params.days;
 		}
-		get_data(dayOffset, write_back);
+		repository.get_data(dayOffset, write_back);
 };
 
 exports.graph = function(req,res) {
@@ -97,6 +93,5 @@ exports.graph = function(req,res) {
 		if(req.params&&req.params.days) {
 			dayOffset=req.params.days;
 		}
-		get_data(dayOffset, write_back);
+		repository.get_data(dayOffset, write_back);
 };
-
